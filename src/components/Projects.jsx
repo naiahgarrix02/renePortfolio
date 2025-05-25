d10daa0c636efa6299aba339c29ef6f73ec82b20
Benaiah Odametey <Benaiah>
2025-05-25 17:06:47 +0000

    update on projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import projectsarrow from "../assets/projectsarrow.svg";
+import "../index.css";
 
 const Projects = () => {
   return (
-    <div className="flex flex-row space-x-100 ">
-      <div>Projects</div>
+    <div className="flex justify-between">
+      <div className="flex flex-col w-[734px] h-[620px] items-center pb-[36px] justify-end rounded-4xl bg-background drop-shadow-xl">
+        <div className="flex w-[654px] justify-between">
+          <h2 className="text-white text-[64px]">the dashboard</h2>
+          <img src={projectsarrow} />
+        </div>
+      </div>
+      <div className="flex flex-col w-[734px] h-[620px] items-center pb-[36px] justify-end rounded-4xl bg-background drop-shadow-xl">
+        <div className="flex w-[654px] justify-between">
+          <h2 className="text-white text-[64px]">the dashboard</h2>
+          <img src={projectsarrow} />
+        </div>
+      </div>
     </div>
   );
 };
